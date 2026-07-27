029730eab12ec8a4c1367288230c524c13881d93
Doan Linh <linhdn.gen@gmail.com>
2026-07-27 15:54:42 +0700

    dsada

--- app/blog/page.test.tsx
+++ app/blog/page.test.tsx
@@ -3,7 +3,7 @@
  */
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Page from "./[slug]/page";
+import Page from "./page";
 
 it("App Router: Works with dynamic route segments", () => {
   render(<Page params={{ slug: "Test" }} />);
